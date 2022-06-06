c337740649789144b2ac2991673d7ea8acef20f3
regheni <reghenieugen@gmail.com>
2022-06-06 11:20:04 +0300

    third commit presenter

--- app.py
+++ app.py
@@ -11,29 +11,29 @@ app = Flask(__name__, template_folder="static")
 
 @app.route("/movies")
 def movie_page():
-    fileData = 0
-    fileData = []
+    file_data = 0
+    file_data = []
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static", "imdb_movie.json")
     with open(json_url) as f:
         for jsonObj in f:
-            movieDict = json.loads(jsonObj)
-            fileData.append(movieDict)
+            movie_dict = json.loads(jsonObj)
+            file_data.append(movie_dict)
     f.close()
     count = -1
-    for line in fileData:
+    for line in file_data:
         count += 1
-        imaages = line["images"]
+        _images = line["images"]
         images_urls = line["image_urls"]
-        if len(imaages) > 0:
-            first_item_images = imaages[0]
+        if len(_images) > 0:
+            first_item_images = _images[0]
             image_url = "media/" + first_item_images["path"]
             line["images"] = image_url
             first_item_image_url = images_urls[0]
             line["image_urls"] = first_item_image_url
-            fileData[count] = line
+            file_data[count] = line
 
-    return render_template("templates/index.html", fileData=fileData)
+    return render_template("templates/index.html", file_data=file_data)
 
 
 if __name__ == "__main__":
