5c8352a4766e5971bf794fe086f20a3a520e5e45
regheni <reghenieugen@gmail.com>
2022-06-06 16:50:29 +0300

    today

--- src/imdbpresenter/app.py
+++ src/imdbpresenter/app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, url_for, redirect, abort
 import json
 import os
 import ipdb
-app = Flask(__name__, template_folder="static")
+
+app = Flask(__name__, template_folder="static", template_folder="./templates")
 
 
 # def validate_input(number):
@@ -10,14 +11,31 @@ app = Flask(__name__, template_folder="static")
 
 
 @app.route("/movies")
+@app.route("/movies/page=<int:page_nr>&items=<int:item_nr>", methods=['GET', 'POST'])
+
+
 def movie_page():
     file_data = 0
     file_data = []
     SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
     json_url = os.path.join(SITE_ROOT, "static", "imdb_movie.json")
+    items_per_page = request.args.get("items", 5, type=int)
+    current_page = request.args.get("page", 1, type=int)
+    page_nr = current_page
+
+    if page_nr == 1:
+        page_position = 0
+    elif page_nr > 1 and page_nr <= len(total_items) // items_per_page:
+        page_position = items_per_page * (page_nr -1)
+    else:
+        page_position = 0
+
+    return render_template("movie.html", total_items[page_position : page_position + items_per_page], current_page=current_page)
+
+
     with open(json_url) as f:
-        for jsonObj in f:
-            movie_dict = json.loads(jsonObj)
+        for json_obj in f:
+            movie_dict = json.loads(json_obj)
             file_data.append(movie_dict)
     f.close()
     count = -1
