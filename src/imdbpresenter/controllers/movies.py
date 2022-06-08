5e82f95c529740e6db37bd9e3b099d423c549de8
regheni <reghenieugen@gmail.com>
2022-06-08 13:01:25 +0300

    new changes

--- src/imdbpresenter/controllers/movies.py
+++ src/imdbpresenter/controllers/movies.py
@@ -3,28 +3,30 @@ import os
 import json
 from imdbpresenter import ROOT_PATH
 from imdbpresenter.models.movie import Movie
+import ipdb
 
 movies_blueprint = Blueprint("movies", __name__)
 
 @movies_blueprint.route("/movies")
 def index():
     all_movies = Movie.all()
-    import ipdb
 
-    ipdb.set_trace()
 
     file_data = []
+    count = 0
 
-    for line in file_data:
-        count += 1
-        _images = line["image"]
-        images_urls = line["image_urls"]
-        if len(_images) > 0:
-            first_item_images = _images [0]
-            image_url = "media/full/" + first_item_images["path"]
-            line["images"] = image_url
-            first_item_image_url = images_urls [0]
-            line["image_urls"] = first_item_image_url
-            file_data[count] = line
+    for line in all_movies:
 
-    return render_template("templates/movies/index.html", file_data = file_data)
+        # count += 1
+        # print(line["images"])
+        # imaages = line["images"]
+        # images_urls = line["image_urls"]
+        # if len(imaages) > 0:
+        #     first_item_images = imaages [0]
+        #     image_url = "media/full/" + first_item_images["path"]
+        #     line["images"] = image_url
+        #     first_item_image_url = images_urls [0]
+        #     line["image_urls"] = first_item_image_url
+        #     file_data[count] = line
+        # ipdb.set_trace()
+        return render_template("templates/movies/index.html", line = line.to_json())
--- src/imdbpresenter/models/movie.py
+++ src/imdbpresenter/models/movie.py
@@ -1,12 +1,14 @@
 from imdbpresenter import ROOT_PATH
 import os
 import json
+import ipdb
 
 JSON_URL = os.path.join(ROOT_PATH, "src/imdbpresenter/static", "imdb_movie.json")
 
 class Movie:
     def __init__(self, **kwargs):
         self.kwargs = kwargs
+
 
     @classmethod
     def all(cls):
@@ -15,11 +17,13 @@ class Movie:
             for json_obj in f:
                 movie_dict = json.loads(json_obj)
                 movie = Movie(**movie_dict)
+                print(movie)
                 all_movies.append(movie)
         return all_movies
 
     def to_json(self):
-        return dict(title=self.title, genres=self.genres)
+        return dict(title=self.kwargs.get("title"), genre=self.kwargs.get("genre"), images=self.kwargs.get("images"))
+
 
     @classmethod
     def first(cls):
